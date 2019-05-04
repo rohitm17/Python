926b765545a3413feb79998b271721d746db84d6
Rohit Mittal <35946128+rohitm17@users.noreply.github.com>
2019-05-04 12:51:43 +0530

    Add files via upload

---
+++ vowel.py
@@ -0,0 +1,5 @@
+x=input("enter the character :")
+if(x=='a' or x=='e' or x=='i' or x=='u' or x=='o' or x=='A' or x=='E' or x=='I' or x=='O' or x=='U' ):
+    print("character entered is vowel")
+else:
+    print("character is not vowel")
