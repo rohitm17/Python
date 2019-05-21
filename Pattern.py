337756e7f9a134e177fdccba59f79ea284e45602
Rohit Mittal <35946128+rohitm17@users.noreply.github.com>
2019-05-21 23:44:34 +0530

    Update Pattern.py

--- Pattern.py
+++ Pattern.py
@@ -106,3 +106,41 @@ for i in range(1,n+1):
 '''
 print("\n$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
 
+print("Print Alphabets and Letters pattern in python ")
+lastNumber = 6
+asciiNumber = 65
+for i in range(0, lastNumber):
+    for j in range(0, i+1):
+        character  = chr(asciiNumber)
+        print(character, end=' ')
+        asciiNumber+=1
+    print(" ")
+'''
+    Print Alphabets and Letters pattern in python 
+    A  
+    B C  
+    D E F  
+    G H I J  
+    K L M N O  
+    P Q R S T U
+'''
+print("$$$$$$$$$$$$$$$$$$$$$$$$$")
+
+decr = 8
+count = 64
+val = 65
+for i in range(0, 5):
+    for k in range(0, decr):
+        print(end=" ")
+    for j in range(0, i+1):
+        count = count + 1
+    val = count
+    temp = val
+    for j in range(0, i+1):
+        ch = chr(val)
+        print(ch, end=" ")
+        val = val - 1
+    val = temp
+    decr = decr - 2
+    print()
+ 
